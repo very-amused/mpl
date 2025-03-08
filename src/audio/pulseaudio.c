486da6d14671d7469c491d5d460f84468e2be5ce
very-amused <very-amused@pm.me>
2025-03-08 17:31:14 -0500

    init/deinit AudioBackend in main

--- src/audio/pulseaudio.c
+++ src/audio/pulseaudio.c
@@ -3,14 +3,27 @@
 typedef struct pa_ctx {
 } pa_ctx;
 
-static int PulseAudio_init(pa_ctx *ctx) {
+static int pa_init(void *ctx__) {
+	pa_ctx *ctx = ctx__;
+
 	// TODO: Set up pa_ctx
 
 	return 0;
 }
 
-AudioBackend AB_PulseAudio = {
+static int pa_deinit(void *ctx__) {
+	pa_ctx *ctx = ctx__;
+
+	// TODO: teardown pa_ctx
+
+	return 0;
+}
+
+AudioBackend *AB_PulseAudio = {
 	.name = "PulseAudio",
+
+	.init = pa_init,
+	.deinit = pa_deinit,
 
 	.ctx_size = sizeof(pa_ctx)
 };
--- src/main.c
+++ src/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "audio/backend.h"
+#include "audio/backends.h"
 
-int main() {
-	printf("This is UMP v0.0.0\n");
+int main(int argc, char **argv) {
+	printf("This is MPL v0.0.0\n");
+
+	if (argc < 2) {
+		fprintf(stderr, "usage: mpl {file}\n");
+		return 1;
+	}
+	char *file = argv[1];
+	
+	// Initialize audio backend
+	AudioBackend ab = AB_PulseAudio;
+	AudioBackend_init(&ab);
+
+	// Cleanup
+	AudioBackend_deinit(&ab);
+
 	return 0;
 }
