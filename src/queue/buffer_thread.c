f657d75248f5105487f62ea259d3ff99d169a8cd
very-amused <very-amused@pm.me>
2025-03-30 00:24:55 -0400

    Replace pthread process with libdill

--- src/queue/buffer_thread.c
+++ src/queue/buffer_thread.c
@@ -1,42 +1,38 @@
 #include "buffer_thread.h"
 #include "audio/track.h"
 #include "error.h"
-#include <errno.h>
 
 #ifdef __unix__
-#include <pthread.h>
 #include <semaphore.h>
+#include "libdill.h"
 #endif
 
 struct BufferThread {
 	AudioTrack *track;
 	AudioTrack *next_track;
 
-	pthread_t *thread;
+	int thread_handle;
 	sem_t pause; // Tell the thread to temporarily stop buffering. The next notification to pause will wake the thread back up
-	sem_t cancel; // Tell the thread to exit
 
 	sem_t done; // Receive confirmation that the thread has handled a message (i.e pause)
 };
 
 BufferThread *BufferThread_new() {
 	BufferThread *bt = malloc(sizeof(BufferThread));
-	bt->track = bt->next_track = NULL;
-	bt->thread = NULL;
+
+	bt->thread_handle = -1;
 
 	// Initialize semaphores
 	sem_init(&bt->pause, 0, 0);
-	sem_init(&bt->cancel, 0, 0);
 	sem_init(&bt->done, 0, 0);
 
 	return bt;
 }
 void BufferThread_free(BufferThread *bt) {
 	// Stop thread
-	if (bt->thread) {
-		sem_post(&bt->cancel);
-		pthread_join(*bt->thread, NULL);
-		bt->thread = NULL;
+	if (bt->thread_handle != -1) {
+		dill_hclose(bt->thread_handle);
+		dill_bundle_wait(bt->thread_handle, 100);
 	}
 
 	// We don't own the track pointers, so we do nothing with those
@@ -44,20 +40,14 @@ void BufferThread_free(BufferThread *bt) {
 	free(bt);
 }
 
-static void *BufferThread_routine(void *args) {
-	BufferThread *bt = args;
-
+static coroutine void BufferThread_routine(BufferThread *bt) {
 	// Start buffering from bt->track
 	enum AudioTrack_ERR at_err;
 buffer_loop:
 	do {
-		int cancelval, pauseval;
+		int pauseval;
 		sem_getvalue(&bt->pause, &pauseval);
-		sem_getvalue(&bt->cancel, &cancelval);
-		if (cancelval) {
-			fprintf(stderr, "canceled\n");
-			pthread_exit(NULL);
-		} else if (pauseval) {
+		if (pauseval) {
 			fprintf(stderr, "paused\n");
 			sem_post(&bt->done);
 			sem_wait(&bt->pause);
@@ -71,10 +61,7 @@ buffer_loop:
 
 	if (!bt->next_track) {
 		// Self-detach
-		pthread_detach(*bt->thread);
-		free(bt->thread);
 		sem_post(&bt->done);
-		pthread_exit(NULL);
 	}
 
 	bt->track = bt->next_track;
@@ -84,7 +71,7 @@ buffer_loop:
 
 int BufferThread_start(BufferThread *bt, AudioTrack *track, AudioTrack *next_track) {
 	// Pause the thread
-	if (bt->thread) {
+	if (bt->thread_handle != -1) {
 		sem_post(&bt->pause);
 		sem_wait(&bt->done);
 	}
@@ -94,13 +81,12 @@ int BufferThread_start(BufferThread *bt, AudioTrack *track, AudioTrack *next_tra
 	bt->next_track = next_track;
 
 	// Start or resume buffering
-	if (bt->thread) {
+	if (bt->thread_handle != -1) {
 		sem_post(&bt->pause);
 		return 0;
 	}
-	bt->thread = malloc(sizeof(pthread_t));
 
-	pthread_create(bt->thread, NULL, BufferThread_routine, bt);
+	bt->thread_handle = dill_go(BufferThread_routine(bt));
 	sem_wait(&bt->done);
 	return 0;
 }
