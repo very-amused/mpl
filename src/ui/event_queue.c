a12b4bd54ba4d40b02184e2b6e61a644457e34bb
very-amused <very-amused@pm.me>
2025-04-07 19:08:12 -0400

    Start working on UI EventQueue

--- src/ui/event_queue.c
+++ src/ui/event_queue.c
@@ -2,9 +2,38 @@
 #include "event_queue.h"
 
 #include <fcntl.h>
+#include <linux/limits.h>
+#include <stdio.h>
 #include <sys/stat.h>
 #include <mqueue.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 struct EventQueue {
-	mqd_t mq;	// Message queue
+	// Message queue handle
+	mqd_t mq; 
 };
+
+static void mq_gen_name_(char *name, size_t name_max) {
+	snprintf(name, name_max, "/mpl/ui/event_queue/%d", getpid());
+}
+
+EventQueue *EventQueue_new() {
+	EventQueue *eq = malloc(sizeof(EventQueue));
+
+	// Generate mq name
+	char mq_name[NAME_MAX];
+	mq_gen_name_(mq_name, NAME_MAX);
+	eq->mq = mq_open(mq_name, O_RDWR | O_CREAT);
+	if (eq->mq == -1) {
+		perror("EventQueue message queue");
+		free(eq);
+		return NULL;
+	}
+
+	return eq;
+}
+void EventQueue_free(EventQueue *eq) {
+	mq_close(eq->mq);
+	free(eq);
+}
