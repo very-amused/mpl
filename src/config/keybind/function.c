7a84174ae778da4ba000055f423209142e9d07a7
very-amused <very-amused@pm.me>
2025-06-09 22:52:27 -0400

    Define BindableFnID enum
    Because switching on function pointers would've been too easy

--- src/config/keybind/function.c
+++ src/config/keybind/function.c
@@ -1,15 +1,79 @@
 #include "function.h"
+#include "config/functions.h"
 
 #include <string.h>
 #include <stddef.h>
 #include <stdlib.h>
 
+enum BindableFnID KeybindFn_getid(const char *ident) {
+	switch (ident[0]) {
+	case 'p':
+		if (strcmp(ident, "play_toggle") == 0) {
+			return bfn_play_toggle;
+		}
+		break;
+	case 's':
+		if (strcmp(ident, "seek") == 0) {
+			return bfn_seek;
+		} else if (strcmp(ident, "seek_snap") == 0) {
+			return bfn_seek_snap;
+		}
+		break;
+	case 'q':
+		if (strcmp(ident, "quit") == 0) {
+			return bfn_quit;
+		}
+		break;
+	}
+
+	return NULL;
+}
+
+KeybindFn KeybindFn_get(enum BindableFnID fn) {
+	switch (fn) {
+	case bfn_play_toggle:
+		return play_toggle;
+	case bfn_quit:
+		return quit;
+	case bfn_seek:
+		return (KeybindFn)seek;
+	case bfn_seek_snap:
+		return (KeybindFn)seek_snap;
+	}
+	return NULL;
+}
+
+enum KeybindMap_ERR KeybindFn_parse_args(enum BindableFnID fn,
+		void **fn_args, KeybindFnArgDestructor **destructor,
+		const char *str, size_t offset) {
+	switch (fn) {
+	case bfn_play_toggle:
+	{
+		// TODO
+		break;
+	}
+	case bfn_quit:
+	{
+		// TODO
+		break;
+	}
+	case bfn_seek:
+	{
+		// TODO
+		break;
+	}
+	case bfn_seek_snap:
+	{
+		// TODO
+		break;
+	}
+	}
+}
+
 void KeybindRoutine_init(KeybindRoutine *routine) {
 	memset(routine, 0, sizeof(KeybindRoutine));
 }
 
-// Deintialize a KeybindRoutine, taking care of running
-// the correct function argument destructors
 void KeybindRoutine_deinit(KeybindRoutine *routine) {
 	// Run destructors to deinitialize and free function args
 	for (size_t i = 0; i < routine->n_fns; i++) {
--- src/config/keybind/function.h
+++ src/config/keybind/function.h
@@ -1,14 +1,41 @@
 #pragma once
 #include "error.h"
 
+#define BINDABLE_FNS(VARIANT) \
+	VARIANT(bfn_play_toggle) \
+	VARIANT(bfn_quit) \
+	VARIANT(bfn_seek) \
+	VARIANT(bfn_seek_snap)
+
+// We can't switch on function pointers,
+// so we'll switch on an enum instead.
+// Pain in the ass...
+enum BindableFnID {
+	BINDABLE_FNS(ENUM_VAL)
+};
+
+static const inline char *BindableFnID_name(enum BindableFnID bfn_id) {
+	switch (bfn_id) {
+		BINDABLE_FNS(ENUM_KEY)
+	}
+	return DEFAULT_ERR_NAME;
+}
+
+#undef BINDABLE_FNS
+
 // A function call that can be used in a keybind routine
 // (these functions may later be usable in other places than just keybinds,
 // but a name like ConfigFn would be too vague)
 //
 // (all KeybindFn's are defined in config/functions.h)
 typedef void (*KeybindFn)(void *);
-// Get a KeybindFn by name
-KeybindFn KeybindFn_byname(const char *name);
+
+// Get a KeybindFn's ID value by its identifier (name)
+// Returns -1 if no KeybindFn with the given identifier exists
+enum BindableFnID KeybindFn_getid(const char *ident);
+// Get a KeybindFn's pointer from its ID value
+// Returns NULL if the ID isn't a valid BindableFnID
+KeybindFn KeybindFn_get(enum BindableFnID fn);
 
 // A destructor function that deinitializes (if applicable) and frees the argument struct
 // passed to a KeybindFn.
@@ -18,7 +45,9 @@ typedef void (*KeybindFnArgDestructor)(void *);
 //
 // Sets *fn_args to point to the function's argument struct.
 // Sets *fn_destructor to point to the function's KeybindFnArgDestructor.
-enum KeybindMap_ERR KeybindFn_parse_args(const KeybindFn fn, void **fn_args, KeybindFnArgDestructor **destructor);
+enum KeybindMap_ERR KeybindFn_parse_args(enum BindableFnID fn,
+		void **fn_args, KeybindFnArgDestructor **destructor,
+		const char *str, size_t offset);
 
 typedef struct KeybindRoutine {
 	// Number of functions (defined in config/functions.h)
