6e5c5237b6b4a3f138320c0381f377b0108c6245
kscr <very-amused@pm.me>
2025-07-05 21:59:59 -0400

    Write getline for w32

--- src/util/compat/string_win32.c
+++ src/util/compat/string_win32.c
@@ -1,6 +1,8 @@
 #include "string_win32.h"
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char *strndup(const char *s, const size_t n) {
     char *copy = malloc((n + 1) + sizeof(char));
@@ -14,4 +16,39 @@ char *strndup(const char *s, const size_t n) {
     copy[n] = '\0';
 
     return copy;
+}
+
+ssize_t getline(char **line, size_t *line_len, FILE *stream) {
+    static const char LF = '\n';
+    // Line buffer used with fgets
+    static char buf[BUFSIZ];
+
+    // Ensure our destination can hold the line contents
+    size_t line_cap = *line_len;
+    if (*line == NULL || line_cap < sizeof(buf)) {
+        line_cap = sizeof(buf);
+        *line = realloc(*line, line_cap);
+    }
+
+    // Use our buffer to read a LF char
+    *line_len = 0;
+    bool has_content = false;
+    while (fgets(buf, sizeof(buf), stream)) {
+        has_content = true;
+        size_t i = 0;
+        for (; buf[i] != '\0'; i++) {
+            (*line)[*line_len] = buf[i];
+            (*line_len)++;
+            if (buf[i] == LF) {
+                return *line_len;
+            }
+        }
+        // We didn't read a newline, so we need to grow *line, refill the buffer, and try again
+        if (line_cap < (*line_len) + sizeof(buf)) {
+            line_cap *= 2;
+            *line = realloc(*line, line_cap);
+        }
+    }
+
+    return has_content ? *line_len : EOF;
 }
