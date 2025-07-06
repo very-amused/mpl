89cd5ce98e39c2dd3c488c90c3a7d81efdd46959
kscr <very-amused@pm.me>
2025-07-05 21:01:52 -0400

    Implement strndup for windows

--- src/util/compat/string_win32.c
+++ src/util/compat/string_win32.c
@@ -1,1 +1,17 @@
-#include <string.h>
+#include "string_win32.h"
+
+#include <stdlib.h>
+
+char *strndup(const char *s, const size_t n) {
+    char *copy = malloc((n + 1) + sizeof(char));
+
+    for (size_t i = 0; i < n; i++) {
+        copy[i] = s[i];
+        if (s[i] == '\0') {
+            break;
+        }
+    }
+    copy[n] = '\0';
+
+    return copy;
+}
