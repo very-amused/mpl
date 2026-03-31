4289c300b319914243abfe282ed0e0379149c8c8
very-amused <very-amused@pm.me>
2026-03-30 21:11:18 -0400

    More work on new EventQueue interface

--- src/ui/event_queue.h
+++ src/ui/event_queue.h
@@ -15,15 +15,28 @@ EventQueue *EventQueue_new();
 // Free an EventQueue allocated using [EventQueue_new]
 void EventQueue_free(EventQueue *eq);
 
-// Send *evt on the EventQueue (*evt is copied).
-// Returns 0 on success, nonzero on error
-// WARN: DEPRECATED in favor of EventSubQueue_send.
-int EventQueue_send(EventQueue *eq, const Event *evt);
+
+// Open a new subqueue that feeds events to *eq.
+// Once returned, this subqueue may be used from a non-main thread.
+// WARN: This routine MUST be called on the main thread.
+EventSubQueue *EventQueue_connect(EventQueue *eq);
+
+// Send an Event via this subqueue to the main EventQueue.
+// Makes a copy of *evt (you don't need to heap-allocate events)
+// NOTE: may block if the subqueue is full. If this isn't desired, pass allow_drop=true,
+// which causes the subqueue to drop new events when it doesn't have room for them.
+void EventSubQueue_send(EventSubQueue *sq, Event *evt, bool allow_drop);
+
 // Wait to receive an event on the EventQueue
 // NOTE: May allocate evt->body, caller is responsible for freeing
 // Returns 0 on success, nonzero on error
 int EventQueue_recv(EventQueue *eq, Event *evt);
 
-// Open a new subqueue that feeds events to *eq.
+/* #region deprecated */
 // WARN: DEPRECATED in favor of EventQueue_connect
 EventQueue *EventQueue_connect_legacy(const EventQueue *eq1, int oflags);
+// Send *evt on the EventQueue (*evt is copied).
+// Returns 0 on success, nonzero on error
+// WARN: DEPRECATED in favor of EventSubQueue_send.
+int EventQueue_send(EventQueue *eq, const Event *evt);
+/* #endregion */
