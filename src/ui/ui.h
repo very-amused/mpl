f13ef132945155e7d6be502a48d093b731874532
very-amused <very-amused@pm.me>
2026-04-06 21:38:56 -0400

    UserInterface_* methods

--- src/ui/ui.h
+++ src/ui/ui.h
@@ -1,7 +1,8 @@
 #pragma once
-#include "config/settings.h"
+#include "config/config.h"
 #include "error.h"
 #include "ui/event_queue.h"
+#include "queue/queue.h"
 
 // Universal UI control methods.
 // Used for keybinds to mutate UI state.
@@ -14,9 +15,10 @@ typedef struct UI_Control UI_Control;
 typedef struct UserInterface UserInterface;
 struct UserInterface {
 	const char *name; // UI name
+	EventQueue *evt_queue; // Main EventQueue
 
-	// Initialize the UI for the main loop to run.
-	enum UserInterface_ERR (*init)(void *ctx, const Settings *settings);
+	// Initialize UI context (including registering control methods)
+	enum UserInterface_ERR (*init)(void *ctx);
 	// Get control methods that expose UI functionality
 	enum UserInterface_ERR (*get_control)(void *ctx);
 	// Deinitialize the UI
@@ -25,7 +27,10 @@ struct UserInterface {
 	// Run the main UI loop. This always runs on the main thread. This MUST
 	// 1. return UserInterface_OK when the mpl_QUIT event is received.
 	// 2. Handle keybinds somehow
-	enum UserInterface_ERR (*mainloop)(void *ctx);
+	enum UserInterface_ERR (*mainloop)(void *ctx,
+			EventQueue *evt_queue,
+			Queue *track_queue,
+			Config *config);
 
 	// Private UI-specific context
 	const size_t ctx_size;
@@ -37,3 +42,11 @@ struct UI_Control {
 	enum UserInterface_ERR (*refresh_metadata)(void *ctx); // Refresh or reprint track metadata
 	enum UserInterface_ERR (*show_config)(void *ctx); // Show the user their active MPL config
 };
+
+// Initialize the UI for the main loop to run.
+enum UserInterface_ERR UserInterface_init(UserInterface *ui);
+// Deinitialize the UI for shutdown
+void UserInterface_deinit(UserInterface *ui);
+
+// Run the main UI loop
+enum UserInterface_ERR UserInterface_mainloop(UserInterface *ui, Queue *track_queue, Config *config);
