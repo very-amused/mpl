56b40a01ef8155982f12ec86fa8747be28397a24
very-amused <very-amused@pm.me>
2026-04-15 02:22:14 -0400

    Remove unused legacy keybind_call method

--- src/config/keybind/keybind.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <wchar.h>
-
-// Call the keybind assigned to {keycode} if defined.
-// Returns 0 if the keybind was defined, 1 if no keybind exists for that key
-int call_keybind(wchar_t keycode);
