d3cd43867707b321bb2fae35f34dca74e3ce6499
very-amused <very-amused@pm.me>
2026-04-27 18:12:18 -0400

    Write a lexer interface

---
+++ src/config/parse_v2/lexer.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "error.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define TOKEN_ENUM(VARIANT) \
+	/* Symbols */ \
+	VARIANT(Tok_EOF) /* End of file */ \
+	VARIANT(Tok_Semi) /* ; */ \
+	VARIANT(Tok_LF)  /* \n */ \
+	VARIANT(Tok_SingleQuote) /* ' */\
+	VARIANT(Tok_DoubleQuote) /* " */ \
+	VARIANT(Tok_EQ) /* = */ \
+	VARIANT(Tok_Lparen) /* ( */ \
+	VARIANT(Tok_Rparen) /* ) */ \
+	VARIANT(Tok_Bind) /* bind (used in keybind definitions) */ \
+	/* Literals */ \
+	VARIANT(Tok_U32Lit) \
+	VARIANT(Tok_StrLit) \
+	VARIANT(Tok_BoolLit) \
+	/* Identifiers */ \
+	VARIANT(Tok_SettingIdent) /* Name of a MPL setting */ \
+	VARIANT(Tok_FnIdent) /* Name of an MPL function */ \
+	/* Key names */ \
+	VARIANT(Tok_Keyname) /* Name of a key to be used in a keybind */
+
+enum LexerToken_t {
+	TOKEN_ENUM(ENUM_VAL)
+};
+
+static inline const char *LexerToken_t_name(enum LexerToken_t tok) {
+	switch (tok) {
+		TOKEN_ENUM(ENUM_KEY)
+	}
+	return DEFAULT_ERR_NAME;
+}
+
+#undef TOKEN_ENUM
+
+// A logical token used in parsing
+typedef struct LexerToken {
+	enum LexerToken_t type;
+	union {
+		uint32_t u32_lit;
+		char *str_lit;
+		bool bool_lit;
+
+		// Identifier for something
+		char *ident;
+	};
+} LexerToken;
+
+// Free a lexer token
+void LexerToken_free(LexerToken *tok);
+
+typedef struct Lexer Lexer;
+
+// Allocate and initialize a new config lexer.
+// Returns NULL on error
+Lexer *Lexer_new();
+// Deinitialize and free a config lexer.
+void Lexer_free(Lexer *l);
+
+// Tokenize a chunk of MPL config.
+// Returns 0 on success, nonzero on error
+int Lexer_tokenize(Lexer *l, const char *chunk);
+
+// Peek the next logical token
+const LexerToken *Lexer_peek(const Lexer *l);
+// Consume the current logical token, causing the lexer to advance to the next token
+void Lexer_advance(Lexer *l);
