c00c74fc6da9b10d43565139585e1b10d20a7c6f
very-amused <very-amused@pm.me>
2026-05-19 18:20:25 -0400

    Shell input on win32

--- src/ui/interface/cli/termio_thread_win32.c
+++ src/ui/interface/cli/termio_thread_win32.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #include <Windows.h>
 #include <consoleapi.h>
@@ -9,12 +11,14 @@
 #include <synchapi.h>
 #include <consoleapi2.h>
 #include <fileapi.h>
+#include <wchar.h>
 
 #include "error.h"
 #include "termio_thread.h"
 #include "termio_events.h"
 #include "track_queue/state.h"
 #include "ui/event.h"
+#include "ui/event_queue.h"
 #include "util/log.h"
 
 struct TermIOThread {
@@ -108,7 +112,7 @@ static char TermIOThread_getchar(TermIOThread *thr) {
 
 		DWORD n_read;
 		LOG(Verbosity_DEBUG, "Blocking on ReadConsole...\n");
-		ReadConsoleA(thr->input, &input_key, sizeof(input_key), &n_read, NULL);
+		ReadConsoleA(thr->input, &input_key, 1, &n_read, NULL);
 		// We have to flush the input buffer to avoid the keypress event re-firing (yes, it is bad that we have to do this)
 		FlushConsoleInputBuffer(thr->input);
 		LOG(Verbosity_DEBUG, "Done with ReadConsole\n");
@@ -119,4 +123,80 @@ static char TermIOThread_getchar(TermIOThread *thr) {
 
 // Enter a readline-powered shell, passing shell line events as they come
 static int TermIOThread_shell(TermIOThread *thr) {
+	write_playback_info(thr, InputMode_SHELL);
+
+	HANDLE object_handles[] = {thr->evt_pipe_sem, thr->input};
+	static const DWORD n_handles = sizeof(object_handles) / sizeof(object_handles[0]);
+
+	while (true) {
+		DWORD status = WaitForMultipleObjects(n_handles, object_handles, false, INFINITE);
+		if (!(status >= WAIT_OBJECT_0 && status <= WAIT_OBJECT_0+(n_handles-1))) {
+			LOG(Verbosity_NORMAL, "WaitForMultipleObjects failed. This should never happen\n");
+			continue;
+		}
+
+		const DWORD signal_index = status - WAIT_OBJECT_0;
+
+		if (signal_index == 0) {
+			TermIO_Event evt;
+			bool ok = ReadFile(thr->evt_pipe_rd, &evt, sizeof(TermIO_Event), NULL, NULL);
+			if (!ok) {
+				LOG(Verbosity_VERBOSE, "Error: evt_pipe_sem desync'd from actual event pipe\n");
+				continue;
+			}
+
+			switch (evt.event_type) {
+				case TermIO_SHUTDOWN:
+					return EOF;
+				case TermIO_CHANGE_MODE:
+					return CONTINUE_IO_LOOP;
+				case TermIO_TIMECODE:
+					{
+						strncpy(thr->timecode_buf, evt.body, sizeof(thr->timecode_buf)-1);
+						strncpy(thr->duration_buf, evt.body2, sizeof(thr->duration_buf)-1);
+						write_playback_info(thr, InputMode_SHELL);
+					}
+					break;
+				case TermIO_PLAYBACK_STATE:
+					{
+						thr->playback_state = evt.body_inline;
+						write_playback_info(thr, InputMode_SHELL);
+					}
+					break;
+			}
+
+			continue;
+		}
+
+		// Read the next character of input
+		wchar_t c;
+		DWORD n_read;
+		ReadConsoleW(thr->input, &c, 1, &n_read, NULL);
+		FlushConsoleInputBuffer(thr->input);
+		// Call any shell-enabled keybind bound to {c}
+		if (KeybindMap_call_keybind(thr->keybinds, c, true) == Keybind_OK) {
+			continue;
+		}
+		// If {c} is not bound to a shell keybind,
+		// return it to the input stream and have readline process it
+		rl_stuff_char(c);
+		rl_callback_read_char();
+	}
+}
+
+static TermIOThread *rl_callback_userdata_ = NULL;
+
+static void rl_line_ready_cb_(char *line) {
+	TermIOThread *thr = rl_callback_userdata_;
+
+	// Send SHELL_CLOSE on EOF
+	if (!line) {
+		const Event evt = {.event_type = mpl_SHELL_CLOSE, .body_size = 0};
+		EventSubQueue_send(thr->evt_sq, &evt, false);
+		return;
+	}
+
+	add_history(line);
+	Event evt = {.event_type = mpl_INPUT_LINE, .body_size = strlen(line), .body = line};
+	EventSubQueue_send(thr->evt_sq, &evt, false);
 }
